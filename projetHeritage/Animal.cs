4bf93d3fd7ec0b0353927877e89649dbbc1975e8
Christ ASSANA <christassana@gmail.com>
2024-06-23 18:41:13 +0200

    Update1

--- projetHeritage/Animal.cs
+++ projetHeritage/Animal.cs
@@ -7,6 +7,7 @@ using System.Xml.Linq;
 
 namespace projetHeritage
 {
+    // Classe abstraite Animal 
     public abstract class Animal
     {
         protected string name;
@@ -19,32 +20,115 @@ namespace projetHeritage
 
         }
 
-        public void Eat()
+        //Methode Manger 
+        public virtual void Eat()
         {
             System.Console.WriteLine(name + " mange");
         }
+
+        //Methode Se deplacer
         public virtual void Move()
         {
             System.Console.WriteLine(name + " se deplace");
         }
+    }
 
-        public class Bipedes : Animal
+    public class Bipedes : Animal
+    {
+        public Bipedes(string _name) : base(_name, 2)
+        { }
+
+        public override void Eat()
         {
-            public Bipedes(string _name) : base (_name, 2)
-            { }
-            public override void Move()
-            { }
-
+            System.Console.WriteLine(name + " utilise ses mains pour manger. ");
         }
-        public class  Human: Bipedes
-        {
-            public Human(string _name) : base(_name, 2)
-            { }
-            public override void Move()
-            {
-                Console.WriteLine(" se déplace sur ces deux jambes. ");
-            }
 
+        public override void Move()
+        {
+            Console.WriteLine(name + " se déplace sur deux pattes. ");
+        }
+
+    }
+    public class Human : Bipedes
+    {
+        public Human(string _name) : base(_name)
+        { }
+
+        public override void Eat()
+        {
+            System.Console.WriteLine(name + " utilise des ustensiles et ses mains pour manger. ");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine(name + " marche sur deux jambes. ");
+        }
+    }
+    public class Singe : Bipedes
+    {
+        public Singe(string _name) : base(_name)
+        { }
+
+        public override void Eat()
+        {
+            Console.WriteLine(name + " utilise ses mains pour manger. ");
+        }
+
+
+        public override void Move()
+        {
+            Console.WriteLine(name + " marche sur deux jambes et aussi avec ses mains. ");
+        }
+    }
+
+
+    public class Quadrupedes : Animal
+    {
+        public Quadrupedes(string _name) : base(_name, 4)
+        { }
+        public override void Eat()
+        {
+            System.Console.WriteLine(name + " utilise ses pattes pour manger. ");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine(" se déplace sur ces quatres pates. ");
+        }
+
+    }
+
+    public class Elephant : Quadrupedes
+    {
+        public Elephant(string _name) : base(_name)
+        { }
+
+        public override void Move()
+        {
+            Console.WriteLine(name + " marche sur ses quatres pates et à une trompe qui peut l'aider. ");
+        }
+    }
+
+    public class Tortue : Quadrupedes
+    {
+        public Tortue(string _name) : base(_name)
+        { }
+
+        public override void Move()
+        {
+            Console.WriteLine(name + " marche sur ses quatres pates mais lentement. ");
+        }
+    }
+
+    public class Lezard : Quadrupedes
+    {
+        public Lezard(string _name) : base(_name)
+        { }
+
+        public override void Move()
+        {
+            Console.WriteLine(name + " marche sur ses quatres pates et très vite. ");
         }
     }
 }
+
